9c6bc00345b91b161cbb8f819dca01dfa99563de
artur <artur@GONZU>
2024-03-17 10:52:01 +0100

    init

--- WzorceProjektowe.API/Program.cs
+++ WzorceProjektowe.API/Program.cs
@@ -12,12 +12,9 @@ namespace WzorceProjektowe.API
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
